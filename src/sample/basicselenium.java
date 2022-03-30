5c3e69826aaa087d7c7457f4b1590cd82b1307a3
Ranjit Patil <ranjeit96@g>
2022-03-30 11:26:59 +0530

    [Abhijeet]-selenium action class code

---
+++ src/sample/basicselenium.java
@@ -0,0 +1,68 @@
+package sample;
+
+
+
+public class basicselenium {
+
+
+		public static void main(String[] args) 
+		{
+			
+			//System.setProperty("webdriver.chrome.driver","C:\\Chromedriver98\\chromedriver_win32\\chromedriver.exe");
+			//WebDriver driver=new ChromeDriver();
+			
+			//driver.get("https://www.flipkart.com/");
+			//driver.manage().window().maximize();
+	      //  driver.findElement(By.xpath("//button[@class='_2KpZ6l _2doB4z']")).click();
+			
+//	      mouse hover to element
+//			WebElement moreele = driver.findElement(By.xpath("//div[@class='exehdJ']"));
+//			Actions act =new Actions(driver);
+//			act.moveToElement(moreele).perform();
+			
+	//Right click on element by using mouse
+//			WebElement rightclick = driver.findElement(By.xpath("//div[@class='exehdJ']"));
+//			Actions a =new Actions(driver);
+			//a.moveToElement(rightclick).contextClick().build().perform();
+			//OR
+	    	//a.contextClick(rightclick).perform();
+			
+	//left click on element by using mouse
+//			WebElement leftclick = driver.findElement(By.xpath("//div[@class='exehdJ']"));
+//			Actions aa =new Actions(driver);
+//			aa.moveToElement(leftclick).click().perform();
+			
+	//double click on element by using mouse
+//			WebElement doubleclick = driver.findElement(By.xpath("//div[@class='exehdJ']"));
+//			Actions aaa =new Actions(driver);
+//			aaa.doubleClick(doubleclick).perform();
+			
+	//Drag and drop==
+//			driver.navigate().to("https://jqueryui.com/droppable/");
+//			WebElement frame = driver.findElement(By.xpath("//iframe[@class='demo-frame']"));
+//			driver.switchTo().frame(frame);
+//			
+//			WebElement dragobject = driver.findElement(By.xpath("//div[@id='draggable']"));
+//			WebElement dropobject = driver.findElement(By.xpath("//div[@id='droppable']"));
+//			Actions acto = new Actions(driver);
+//			acto.dragAndDrop(dragobject, dropobject).perform();
+	//slid//er==	
+			//driver.navigate().to("https://jqueryui.com/slider/");
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+
+
+
+	}
+
